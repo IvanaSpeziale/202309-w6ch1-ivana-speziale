0d8343a68f3257ea883ba1d442ca1c65808d8868
IvanaSpeziale <139582321+IvanaSpeziale@users.noreply.github.com>
2023-11-13 21:09:43 +0100

    Add config and phone example

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/app/App.tsx';
+import './style.scss';
+import { AppContextProvider } from './context/app.provider.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <AppContextProvider>
+      <App />
+    </AppContextProvider>
+  </React.StrictMode>
+);
